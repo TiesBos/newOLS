8888ea2c0c11af2424e19ca9370937743b2133ec
Ties Bos <ties.bos2002@gmail.com>
2024-05-05 12:31:14 +0200

    Functions Update

--- R/funcOLS.R
+++ R/funcOLS.R
@@ -16,16 +16,31 @@
 #' n <- 100
 #' X1 <- rnorm(n)
 #' X2 <- rnorm(n)
-#' y <- 1.5*X1 + 1.5\*X2+rnorm(n)
+#' y <- X1 + 1.5*X2+rnorm(n)
 #' beta_OLS2 <- olsFunc(y~X1+X2)
 #'
 #' df <- data.frame(y,X1, X2)
 #' colnames(df) <- c("Y", "A", "B")
 #' beta_OLS3 <- olsFunc(Y~A+B, data=df)
 olsFunc <- function(formula, data=NULL, intercept=TRUE){
-  all.vars(formula)
+  var_names <- all.vars(formula)
 
   if(is.null(data)){
-    h
+    vars <- do.call(cbind,lapply(var_names, get))
+  } else{
+    vars <- data[, var_names]
   }
+
+  y <- vars[,1]
+  if(intercept){
+    X <- cbind(1, vars[,-1])
+    colnames(X) <- c("intercept", var_names[-1])
+  } else {
+    X <- vars[,-1]
+    colnames(X) <- c(var_names[-1])
+  }
+
+  beta <- solve(t(X)%*%X)%*%t(X)%*%y
+  rownames(beta) <- colnames(X)
+  return(beta)
 }
