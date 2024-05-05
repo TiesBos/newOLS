98aa64cadfa18bc6e04ea00c04ae435ffa4a4f6a
Ties Bos <ties.bos2002@gmail.com>
2024-05-05 11:24:13 +0200

    Set Up

---
+++ R/funcOLS.R
@@ -0,0 +1,12 @@
+#' Function for OLS Estimators
+#'
+#' @param formula An object of class \link[stats:formula]{stats::formula}
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+olsFunc <- function(formula, data=NULL){
+
+}
