89104f02b1fe0cab41a6a71d451b6f470b4f5a5e
Ties Bos <ties.bos2002@gmail.com>
2024-05-05 11:25:28 +0200

    Set Up 2

--- R/funcOLS.R
+++ R/funcOLS.R
@@ -1,12 +1,23 @@
 #' Function for OLS Estimators
 #'
 #' @param formula An object of class \link[stats:formula]{stats::formula}
-#' @param data
+#' @param data an optional data frame, list or environment containing the variables in the model.
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' n <- 100
+#' X <- rnorm(n)
+#' y <- 1.5*X+rnorm(n)
+#' beta_OLS <- olsFunc(y~X)
+#'
+#' n <- 100
+#' X1 <- rnorm(n)
+#' x2 <- rnorm(n)
+#' y <- 1.5*X1 + 1.5\*X2+rnorm(n)
+#' df <- data.frame(y,X1, X2)
+#' beta_OLS3 <- olsFunc(y~X1+X2, data=df)
 olsFunc <- function(formula, data=NULL){
 
 }
